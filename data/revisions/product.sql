07509ccd3c49a999d880da5b384f0a2d2c614f9b
emon <emonmr@gmail.com>
2015-01-27 16:39:58 +0600

    some changes

--- data/revisions/product.sql
+++ data/revisions/product.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `products` CHANGE `price` `prices` DECIMAL(8,2) NOT NULL COMMENT 'price of that product';
+ALTER TABLE `products` CHANGE `prices` `price` DECIMAL(8,2) NOT NULL COMMENT 'price of that product';
